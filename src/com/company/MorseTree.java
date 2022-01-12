d6e7bd350abf9b73786538a9acae1806b96fa24f
Xiaolequan <stevenli2088@hotmail.com>
2022-01-12 14:32:55 -0800

    dot

--- src/com/company/MorseTree.java
+++ src/com/company/MorseTree.java
@@ -1,4 +1,26 @@
 package com.company;
 
 public class MorseTree {
+    MorseTreeNode root;
+
+    public MorseTree() {
+        root = new MorseTreeNode('\0');
+        root.dot = new MorseTreeNode('E');
+        root.dot.dot = new MorseTreeNode('M');
+        root.dot.dot.dot = new MorseTreeNode('O');
+        root.dot.dot.dot.dot = new MorseTreeNode('-');
+        root.dot.dot.dot.dot.dot = new MorseTreeNode('0');
+        root.dot.dot.dot.dot.dash = new MorseTreeNode('9');
+        root.dot = new MorseTreeNode('E');
+        root.dot = new MorseTreeNode('E');
+        root.dot = new MorseTreeNode('E');
+        root.dot = new MorseTreeNode('E');
+        root.dot = new MorseTreeNode('E');
+        root.dot = new MorseTreeNode('E');
+        root.dot = new MorseTreeNode('E');
+        root.dot = new MorseTreeNode('E');
+        root.dot = new MorseTreeNode('E');
+        root.dot = new MorseTreeNode('E');
+
+    }
 }
