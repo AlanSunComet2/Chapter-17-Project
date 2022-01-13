8de55f3c2490c756347e07f8c93118a448c0d525
Xiaolequan <stevenli2088@hotmail.com>
2022-01-13 13:10:23 -0800

    dot

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,22 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
-    public static void main(String[] args) {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader);
+    public static void main(String[] args) throws FileNotFoundException {
+        //BufferedReader bufferedReader = new BufferedReader(new FileReader(""));
+        MorseTree mTree = new MorseTree();
+       System.out.println(mTree.find('L'));
     }
+
+   // public static String decode(String morse){
+   //     String decoded = "";
+       // MorseTreeNode node = new MorseTreeNode();
+   //     for (int i = 0; i < morse.length(); i++) {
+           // if (morse.charAt(i))
+  //      }
+
+//    }
 }
--- src/com/company/MorseTree.java
+++ src/com/company/MorseTree.java
@@ -2,8 +2,10 @@ package com.company;
 
 public class MorseTree {
     MorseTreeNode root;
-
+    char[] key = {'E', 'I', 'A'};
+    int level = 0;
     public MorseTree() {
+        root = new MorseTreeNode('\0');
         root.dot = new MorseTreeNode('E');
         root.dot.dot = new MorseTreeNode('I');
         root.dot.dot.dot = new MorseTreeNode('S');
@@ -14,7 +16,7 @@ public class MorseTree {
         root.dot.dash.dash.dash = new MorseTreeNode('J');
         root.dot.dash.dash.dash.dash = new MorseTreeNode('1');
         root.dot.dot.dash = new MorseTreeNode('U');
-        root.dot.dot.dash.dash = new MorseTreeNode('');
+        root.dot.dot.dash.dash = new MorseTreeNode('\0');
         root.dot.dot.dash.dash.dash = new MorseTreeNode('2');
         root.dot.dot.dash.dot = new MorseTreeNode('F');
         root.dot.dot.dot.dash = new MorseTreeNode('V');
@@ -22,7 +24,7 @@ public class MorseTree {
         root.dot.dot.dot.dot.dash = new MorseTreeNode('4');
         root.dot.dash.dot = new MorseTreeNode('R');
         root.dot.dash.dot.dot = new MorseTreeNode('L');
-        root.dot.dash.dot.dash = new MorseTreeNode('');
+        root.dot.dash.dot.dash = new MorseTreeNode('\0');
         root.dot.dash.dot.dash.dot = new MorseTreeNode('+');
         root.dot.dash.dash.dot = new MorseTreeNode('P');
         root.dash = new MorseTreeNode('T');
@@ -37,13 +39,46 @@ public class MorseTree {
         root.dash.dot.dot.dot.dot = new MorseTreeNode('6');
         root.dash.dot.dot.dot.dash = new MorseTreeNode('=');
         root.dash.dot.dot.dash.dot = new MorseTreeNode('/');
-        root.dash.dash.dash.dot = new MorseTreeNode('');
+        root.dash.dash.dash.dot = new MorseTreeNode('\0');
         root.dash.dash.dash.dot.dot = new MorseTreeNode('8');
-        root.dash.dash.dash.dash = new MorseTreeNode('');
+        root.dash.dash.dash.dash = new MorseTreeNode('\0');
         root.dash.dash.dash.dash.dash = new MorseTreeNode('0');
         root.dash.dash.dash.dash.dot = new MorseTreeNode('9');
         root.dash.dash.dot.dot = new MorseTreeNode('Z');
         root.dash.dash.dot.dash = new MorseTreeNode('Q');
         root.dash.dash.dot.dot.dot = new MorseTreeNode('7');
     }
+
+    public String find(char Alpha) {
+        String path = "";
+        return find(root, Alpha, path);
+
+    }
+
+    private String find(MorseTreeNode node, char Alpha, String path) {
+        if (node == null) {
+            return null;
+        }
+        if (node.data == Alpha) {
+            return path;
+        }
+        if (node.dot != null)
+            path = find(node.dot, Alpha, path + ". ");
+        if (node.dash != null)
+            path = find(node.dash, Alpha, path + "- ");
+        findLevel(root,Alpha, 1);
+
+        return path.substring(path.length()-level*2);
+    }
+    private int findLevel(MorseTreeNode node, char Alpha, int height){
+        if(Alpha == node.data){
+            level = height;
+        }
+        if(node.dot!=null)
+            findLevel(node.dot,Alpha,height+1);
+        if(node.dash!= null)
+             findLevel(node.dash, Alpha,height+1);
+        return 0;
+    }
 }
+
